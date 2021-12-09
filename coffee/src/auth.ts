2c87ba2c262c3702e352528a6b8800541f5a2887
Rayza <rayza.ocr@gmail.com>
2021-12-09 00:04:52 -0300

    delete project

--- coffee/src/auth.ts
+++
@@ -1,5 +0,0 @@
-export const signIn = () => localStorage.setItem("token", "my-token");
-
-export const signOut = () => localStorage.removeItem("token");
-
-export const isAuthenticated = () => !!localStorage.getItem("token");
